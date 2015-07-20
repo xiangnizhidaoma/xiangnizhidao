d2890612cc2ab2b74c2de4953c7f48456f48e49b
麒 吴 <573911510@qq.com>
2015-07-20 09:23:23 +0800

    测试中

---
+++ 22223333/22223333/RootView.h
@@ -0,0 +1,13 @@
+//
+//  RootView.h
+//  22223333
+//
+//  Created by 麒 吴 on 15/7/20.
+//  Copyright (c) 2015年 牛方路. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootView : UIView
+
+@end
---
+++ 22223333/22223333/RootView.m
@@ -0,0 +1,21 @@
+//
+//  RootView.m
+//  22223333
+//
+//  Created by 麒 吴 on 15/7/20.
+//  Copyright (c) 2015年 牛方路. All rights reserved.
+//
+
+#import "RootView.h"
+
+@implementation RootView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
